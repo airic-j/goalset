c5053ce066634d37be7fbb226ce8b09f55858911
Eric Johnson <eric@ericslab.com>
2016-06-03 13:18:52 -0700

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,8 @@
+console.log('its alive')
+
+$(function(){
+        $("#typingText").typed({
+            strings: ["Build first website<br>by August 31 ^1000", "Run 4 hour marathon<br>by November 1^1000", "Gym 4 days a week<br>in 2016^1000", "What's your goal<br>and deadline?"],
+            typeSpeed: 120
+        });
+    });
